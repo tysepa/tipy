1954b2a58872e2b902b2b28d655162f5e89c6b02
Epaphrodis <45901123+tysepa@users.noreply.github.com>
2022-11-11 20:52:30 +0200

    Learn react from scrash and quick learning

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="link">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
